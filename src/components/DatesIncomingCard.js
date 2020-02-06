2b58aba1f863c679d96c6441444c074fcae1e642
BrittneyHaselmayer <brittneyhaselmayer@yahoo.com>
2020-02-06 12:57:29 -0600

    mvp

--- src/components/DatesIncomingCard.js
+++ src/components/DatesIncomingCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 
@@ -24,29 +24,43 @@ const useStyles = makeStyles({
 	}
 });
 
-export default function DatesIncomingCard() {
+export default function DatesIncomingCard(props) {
+	// console.log(props);
+	const [user, setUser] = useState({});
+	useEffect(() => {
+		fetch('http://localhost:3000/users')
+			.then((resp) => resp.json())
+			.then((data) => findNeededUser(data));
+		// eslint-disable-next-line
+	}, []);
+
+	const findNeededUser = (data) => {
+		let neededUser = data.find((user) => user.id === props.date.alibi_1_id);
+		setUser(neededUser);
+	};
+
+	const returnName = (user) => {
+		if (user) {
+			return <h1>{user.name}</h1>;
+		}
+	};
+
 	const classes = useStyles();
 	return (
 		<div>
 			<div> YOU ACCEPTED!!</div>
 			<Card className={classes.card} variant="outlined">
 				<CardContent>
-					<Typography
-						className={classes.title}
-						color="textSecondary"
-						gutterBottom
-					>
-						Event
-					</Typography>
+					<Typography gutterBottom>{returnName(user)}</Typography>
 					<Typography className={classes.pos} color="textSecondary">
 						Go to:
 					</Typography>
 					<Typography variant="h5" component="h2">
-						{/* {props.event.occasion} */}
+						{props.date.event.occasion}
 					</Typography>
 
 					<Typography variant="body2" component="p">
-						description at some point
+						On: {props.date.date}
 					</Typography>
 				</CardContent>
 			</Card>
